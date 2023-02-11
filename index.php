6ad984a6beb26e741a2f7733cb70f4045580e928
NamChivas <namt9684@gmail.com>
2023-02-11 07:14:12 +0700

    Edit

--- index.php
+++ index.php
@@ -1,12 +1,57 @@
 <?php
-
 require 'vendor/autoload.php';
 require_once('./vendor/pear/http_request2/HTTP/Request2.php');
+
+
+/** */
+$vlxxID = 7;
+
+$link = getLink($vlxxID);
+
+if ($link == "") {
+    echo "Link is invalid";
+    return;
+}
+
+
+//Download
+file_put_contents("video.mp4", file_get_contents($link));
+
+//Upload
+$file_name = $vlxxID . '.mp4';
+$file_path = './video.mp4';
+
+$cfile = new CURLFile($file_path, mime_content_type($file_path), $file_name);
+$post = array('file' => $cfile);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'http://up.hydrax.net/7a449dc98ca689fc19335da93157573a');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, true);
+$result = curl_exec($ch);
+curl_close($ch);
+
+try {
+    $result = json_decode($result);
+    if ($result->status == true) {
+        file_put_contents('data.txt', $vlxxID . "|" . $result->slug, FILE_APPEND | LOCK_EX);
+        echo "SUCCESS !";
+    }
+} catch (\Throwable $th) {
+    echo "ERROR !";
+    print_r($result);
+    echo $th->getMessage();
+}
+
+
+
 
 /**
  * Function get link
  */
-function getLink($vlxxID)
+function getLink($v_id)
 {
     $mRes = '';
     $request = new HTTP_Request2();
@@ -14,7 +59,7 @@ function getLink($vlxxID)
     $request->setMethod(HTTP_Request2::METHOD_POST);
     $request->addPostParameter('vlxx_server', 1);
     $request->addPostParameter('server', 1);
-    $request->addPostParameter('id', $vlxxID);
+    $request->addPostParameter('id', $v_id);
     $request->setConfig(array('follow_redirects' => TRUE));
     try {
         $response = $request->send();
@@ -41,7 +86,3 @@ function getLink($vlxxID)
     }
     return $mRes;
 }
-
-
-
-print_r(getLink(5));
